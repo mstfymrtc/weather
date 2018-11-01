8d7cde5a2445c9857d60925f90019154edfa6118
Mustafa Yumurtacı <mustafa.yumurtaci@hotmail.com>
2018-11-01 10:45:31 +0300

    axios implemented

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API_KEY = "dd94bd7476ad5690add1313c5facc4a4";
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},tr`;
+  const request = axios.get(url);
+
+  return {
+    type: "FETCH_WEATHER",
+    payload: request
+  };
+}
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+    this.onInputChange = this.onInputChange.bind(this);
+    //TODO:take the existing function(right side), bind it to this & replace it with existing func.(left side)
+    //BUNA ALTERNATİF OLARAK AŞAĞIDA onchange de ()=>this.onInputChange() olarak kullanabilirdik.
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+  onFormSubmit(event) {
+    event.preventDefault();
+    //tells browser not to submit the form
+    //burada apiden veri çekmemiz gerek
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          value={this.state.term}
+          onChange={this.onInputChange}
+          placeholder="Get a five-day forecast in your favorite cities"
+          //TODO:form-control, inputu butona kadar uzattı !
+          className="form-control"
+        />
+        <span className="input-group-btn">
+          <button className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
